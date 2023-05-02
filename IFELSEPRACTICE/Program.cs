4e9a524a1c25aa6011b4b152ff195b1285b8315a
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-05-02 11:12:55 +0500

    Update 02-05-2023

--- IFELSEPRACTICE/Program.cs
+++ IFELSEPRACTICE/Program.cs
@@ -1,15 +1,15 @@
-﻿int english, urdu, islamicStudies, pakStudies, sumOfAllSubjects;
-string e, u, i, p,s;
+﻿int english, urdu, islamicStudies, pakStudies, sumOfAllSubjects; //Variables for input values
+string e, u, i, p,s; //Variables for pass or fail
 Console.Write("Enter Marks of English(1-100):");
-english = Convert.ToInt32(Console.ReadLine());
+english = Convert.ToInt32(Console.ReadLine());  //Input for English Marks
 Console.Write("Enter Marks of Urdu(1-100):");
-urdu = Convert.ToInt32(Console.ReadLine());
+urdu = Convert.ToInt32(Console.ReadLine()); //input for Urdu Marks
 Console.Write("Enter Marks of Islamic Studies(1-100):");
-islamicStudies = Convert.ToInt32(Console.ReadLine());
+islamicStudies = Convert.ToInt32(Console.ReadLine());   //input for Islamic Studies Marks
 Console.Write("Enter Marks of Pak Studies(1-100):");
-pakStudies = Convert.ToInt32(Console.ReadLine());
-sumOfAllSubjects = english+urdu+islamicStudies+pakStudies;
-if (english < 30)
+pakStudies = Convert.ToInt32(Console.ReadLine());   //input For Pak Studies Marks
+sumOfAllSubjects = english+urdu+islamicStudies+pakStudies;  //sum of all marks
+if (english < 30)   //Condition for English
 {
     e = "F";
 }
@@ -17,7 +17,7 @@ else
 {
     e = "P";
 }
-if (urdu < 30)
+if (urdu < 30)  //Condition for Urdu
 {
     u = "F";
 }
@@ -25,7 +25,7 @@ else
 {
     u = "P";
 }
-if (islamicStudies < 30)
+if (islamicStudies < 30)    //Condition For Islamic Studies
 {
     i = "F";
 }
@@ -33,7 +33,7 @@ else
 {
     i = "P";
 }
-if (pakStudies < 30)
+if (pakStudies < 30)    //Condition For Pak Studies
 {
     p = "F";
 }
@@ -41,14 +41,14 @@ else
 {
     p = "P";
 }
-if (english > 30 && urdu > 30 && islamicStudies > 30 && pakStudies > 30) 
+if (english > 30 && urdu > 30 && islamicStudies > 30 && pakStudies > 30) // Condition for all subjects pass
 {
     s = "P";
 }
 else{
     s = "F";
 }
-Console.WriteLine("Your Obtained Marks are Below");
+Console.WriteLine("Your Obtained Marks are Below"); //Result of Marks
 Console.WriteLine(" English     |   Obtained Marks    |   Pass Or Fail");
 Console.WriteLine(" English     |   " + english + "                |   " + e);
 Console.WriteLine(" Urdu        |   " + urdu + "                |   " + u);
@@ -56,5 +56,5 @@ Console.WriteLine(" Islamic     |   " + islamicStudies + "                |   " 
 Console.WriteLine(" PakStudies  |   " + pakStudies + "                |   " + p);
 Console.WriteLine("__________________________________");
 Console.WriteLine(" TotalMarks  |   " + sumOfAllSubjects + "               |   " + s);
-Console.Write("Press Any Key To exit....");
+Console.Write("Press Any Key To exit...."); //Ending Message
 Console.ReadKey();
