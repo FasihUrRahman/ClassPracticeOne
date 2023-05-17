47bf401d5a55cc01b517e02731a6fbe2dd8c1139
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-05-17 11:26:29 +0500

    Update 17-05-2023

---
+++ FindKeyWordInParagraph/Program.cs
@@ -0,0 +1,42 @@
+﻿//Find KeyWord In A Sentence
+//Variables
+string? inputValue, keywordToFind;
+int[] keyWordsFound = new int[20];
+int keywordPosition = 0, arrayIndexPosition = 0;
+//Getting Values From User
+Console.WriteLine("Enter Your Messages Here");
+Console.Write(">>");
+inputValue = Console.ReadLine();
+Console.Write("Enter The Keyword You are Looking For: ");
+keywordToFind = Console.ReadLine();
+
+//If The User Gives Both Values
+if (keywordToFind != "" && inputValue != "")
+{
+    for (int i = keywordPosition; i < inputValue.Length; i++)
+    {
+        if (inputValue[i] == keywordToFind[0])
+        {
+            keyWordsFound[arrayIndexPosition] = inputValue.IndexOf(keywordToFind, i);
+            keywordPosition = i++;
+            arrayIndexPosition++;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Please Input Value First");
+}
+for(int i = 0; i < keyWordsFound.Length; i++)
+{
+    if (keyWordsFound[i] == 0 && i !=0)
+    {
+        break;
+    }
+    else 
+    {
+        Console.WriteLine($"{keywordToFind} This Keyword is Found {keyWordsFound[i]}");
+    }
+}
+Console.ReadLine();
+
