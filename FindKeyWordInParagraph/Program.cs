a770b2b6ae68553eb3dc20846d224fbadb9644ca
FasihUrRahman <fasihurrahmandeveloper@gmail.com>
2023-05-17 12:13:30 +0500

    Update 17-05-2023

--- FindKeyWordInParagraph/Program.cs
+++ FindKeyWordInParagraph/Program.cs
@@ -10,33 +10,46 @@ inputValue = Console.ReadLine();
 Console.Write("Enter The Keyword You are Looking For: ");
 keywordToFind = Console.ReadLine();
 
+
+for (int i = 0; i < inputValue.Length; i++)
+{
+    if (inputValue.IndexOf(keywordToFind, i) == i)
+    {
+        Console.WriteLine($"{keywordToFind} is Available at {i}");
+    }
+}
+
+
 //If The User Gives Both Values
-if (keywordToFind != "" && inputValue != "")
-{
-    for (int i = keywordPosition; i < inputValue.Length; i++)
-    {
-        if (inputValue[i] == keywordToFind[0])
-        {
-            keyWordsFound[arrayIndexPosition] = inputValue.IndexOf(keywordToFind, i);
-            keywordPosition = i++;
-            arrayIndexPosition++;
-        }
-    }
-}
-else
-{
-    Console.WriteLine("Please Input Value First");
-}
-for(int i = 0; i < keyWordsFound.Length; i++)
-{
-    if (keyWordsFound[i] == 0 && i !=0)
-    {
-        break;
-    }
-    else 
-    {
-        Console.WriteLine($"{keywordToFind} This Keyword is Found {keyWordsFound[i]}");
-    }
-}
+//if (keywordToFind != "" && inputValue != "")
+//{
+//    for (int i = keywordPosition; i < inputValue.Length; i++)
+//    {
+//        if (inputValue[i] == keywordToFind[0])
+//        {
+//            keyWordsFound[arrayIndexPosition] = inputValue.IndexOf(keywordToFind, i);
+//            keywordPosition = i++;
+//            arrayIndexPosition++;
+//        }
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Please Input Value First");
+//}
+
+
+//for(int i = 0; i < keyWordsFound.Length; i++)
+//{
+//    if (keyWordsFound[i] == 0 && i !=0)
+//    {
+//        break;
+//    }
+//    else 
+//    {
+//        Console.WriteLine($"{keywordToFind} This Keyword is Found {keyWordsFound[i]}");
+//    }
+//}
+
 Console.ReadLine();
 
